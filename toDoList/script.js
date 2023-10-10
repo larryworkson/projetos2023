de0a23f0248e5297ddbd54279c14826da2e4b6ab
Larry Rocha <143459283+larryworkson@users.noreply.github.com>
2023-10-09 21:40:49 -0300

    dark mode 90%

--- toDoList/script.js
+++ toDoList/script.js
@@ -32,14 +32,34 @@ function mostrar() {
     }
 }
 
-//verifica se já existe algum item na lista para ser editado
 
 function editar(id) {
     let novoconteudo = document.getElementById(`tarefa${id}`).value
     lista[id].task = novoconteudo
-
     mostrar()
 }
-for (let a of lista) {
-    console.log(a)
+
+
+function darkmode(){
+    let mudarestilo = document.getElementById('mudarestilo')
+    let postit = document.getElementsByClassName('postit')
+    let txtpostit = document.getElementsByClassName('novastarefas')
+    let txtinputaarefa = document.querySelector('input#tarefa')
+    document.body.style.backgroundColor = 'rgb(65, 65, 65)'
+    document.body.style.color = 'rgb(255, 255, 255)'
+    txtinputaarefa.style.color = 'rgb(220, 220, 220)'
+    mudarestilo.innerHTML = '<img onclick="lightmode()" src="https://static.vecteezy.com/system/resources/previews/009/266/750/original/sun-icon-design-free-png.png">'
+    for (let v = 0; v < postit.length ; v++) {
+        postit[v].style.backgroundColor = 'rgb(75, 75, 75)'
+    }
+    for (let c = 0 ; c < txtpostit.length ; c++){
+        txtpostit[c].style.color = 'rgb(220, 220, 220)'
+    }
+    
+}
+function lightmode() {
+    let mudarestilo = document.getElementById('mudarestilo')
+    document.body.style.backgroundColor = 'rgb(255, 255, 255)'
+    document.body.style.color = 'rgb(65, 65, 65)'
+    mudarestilo.innerHTML = '<img onclick="darkmode()" src="https://cdn-icons-png.flaticon.com/512/4445/4445942.png">'
 }
